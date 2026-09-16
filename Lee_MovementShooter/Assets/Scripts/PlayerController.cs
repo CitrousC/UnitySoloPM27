8144c32bcbecae6110f58743c375ea17db2933fd
CitrousC <zacharydjlee@gmail.com>
2026-09-16 13:43:54 -0700

    Firing WIP

--- Lee_MovementShooter/Assets/Scripts/PlayerController.cs
+++ Lee_MovementShooter/Assets/Scripts/PlayerController.cs
@@ -7,52 +7,60 @@ public class PlayerController : MonoBehaviour
     public float jumpHeight = 10.0f;
     public float jumpDetectDistance = 1f;
 
-    Vector2 moveInput = Vector2.zero;
     Ray jumpRay;
+    Vector2 moveInput = Vector2.zero;
 
+    Camera playerCam;
     PlayerInput input;
     Rigidbody rb;
+
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         input = GetComponent<PlayerInput>();
         rb = GetComponent<Rigidbody>();
-
         jumpRay = new Ray();
+        playerCam = Camera.main;
+
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+
+    private void FixedUpdate()
+    {
+        Quaternion playerRotation = Quaternion.identity;
+        playerRotation.y = playerCam.transform.rotation.y;
+        playerRotation.w = playerCam.transform.rotation.w;
+        transform.rotation = playerRotation;
     }
 
     // Update is called once per frame
     void Update()
     {
+
+        jumpRay.origin = transform.position;
+        jumpRay.direction = -transform.up;
+
         Vector3 tempMove = rb.linearVelocity;
 
         tempMove.x = moveInput.x * speed;
         tempMove.z = moveInput.y * speed;
 
-        rb.linearVelocity = tempMove;
-
-        jumpRay.origin = transform.position;
-        jumpRay.direction = -transform.up;
-
+        rb.linearVelocity = (tempMove.x * transform.right) +
+                            (tempMove.y * transform.up) +
+                            (tempMove.z * transform.forward);
     }
+
     public void Move(InputAction.CallbackContext context)
     {
         moveInput = context.ReadValue<Vector2>();
     }
 
-    public void Jump ()
+    public void Jump()
     {
         if (Physics.Raycast(jumpRay, jumpDetectDistance))
-        rb.AddForce(transform.up * jumpHeight, ForceMode.Impulse);
-        
-    
-    
+            rb.AddForce(transform.up * jumpHeight, ForceMode.Impulse);
     }
 
-
-
-
 }
-
-
